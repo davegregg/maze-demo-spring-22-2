2c79c1111d053745b99164b3e244f2eb50fcf6ea
davegregg@gmail.com <davegregg@gmail.com>
2022-05-26 13:15:47 -0400

    Demo code, day one

--- code.js
+++ code.js
@@ -1,19 +1,47 @@
-const map = [
-  "WWWWWWWWWWWWWWWWWWWWW",
-  "W   W     W     W   W",
-  "W W W WWW WWWWW W WWW",
-  "W W W  W      W W   W",
-  "W WWWWWWW W WWW W W W",
-  "W         W     W W W",
-  "W WWW WWWWW WWWWW W W",
-  "W W   W   W W     W W",
-  "W WWWWW W W W WWW W F",
-  "S     W W W W W W WWW",
-  "WWWWW W W W W W W W W",
-  "W     W W W   W W W W",
-  "W WWWWWWW WWWWW W W W",
-  "W       W       W   W",
-  "WWWWWWWWWWWWWWWWWWWWW"
-];
+const legend = {
+  "▓": "wall",
+  "░": "floor",
+  "S": "start",
+  "F": "finish",
+}
 
-// Your Code Here.
+const mazeModel = [
+  "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓",
+  "▓░░░▓░░░░░▓░░░░░▓░░░▓",
+  "▓░▓░▓░▓▓▓░▓▓▓▓▓░▓░▓▓▓",
+  "▓░▓░▓░░▓░░░░░░▓░▓░░░▓",
+  "▓░▓▓▓▓▓▓▓░▓░▓▓▓░▓░▓░▓",
+  "▓░░░░░░░░░▓░░░░░▓░▓░▓",
+  "▓░▓▓▓░▓▓▓▓▓░▓▓▓▓▓░▓░▓",
+  "▓░▓░░░▓░░░▓░▓░░░░░▓░▓",
+  "▓░▓▓▓▓▓░▓░▓░▓░▓▓▓░▓░F",
+  "S░░░░░▓░▓░▓░▓░▓░▓░▓▓▓",
+  "▓▓▓▓▓░▓░▓░▓░▓░▓░▓░▓░▓",
+  "▓░░░░░▓░▓░▓░░░▓░▓░▓░▓",
+  "▓░▓▓▓▓▓▓▓░▓▓▓▓▓░▓░▓░▓",
+  "▓░░░░░░░▓░░░░░░░▓░░░▓",
+  "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+]
+
+const mazeView = document.querySelector("main")
+
+for (let rowIndex = 0; rowIndex < mazeModel.length; rowIndex += 1) {
+  const rowModel = mazeModel[rowIndex]
+  const rowView = document.createElement("div")
+  rowView.classList.add("row")
+  rowView.dataset.row = rowIndex
+
+  mazeView.append(rowView)
+  
+  for (let cellIndex = 0; cellIndex < rowModel.length; cellIndex += 1) {
+    const cellModel = rowModel[cellIndex]
+    const cellView = document.createElement("div")
+    cellView.classList.add("cell")
+    cellView.dataset.row = rowIndex
+    cellView.dataset.cell = cellIndex
+    
+    rowView.append(cellView)
+    
+    console.log({ cellModel, rowIndex, cellIndex })
+  }
+}
