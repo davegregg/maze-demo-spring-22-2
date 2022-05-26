d6c610b08662dd2c221f0bfb712d6b7f04a945a3
davegregg@gmail.com <davegregg@gmail.com>
2022-05-26 12:23:51 -0400

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kenzie Academy Maze Activity
+
+Complete your work inside of `code.js`
+
+Follow the instructions provided on my.kenzie.academy for this assignment.
---
+++ code.js
@@ -0,0 +1,19 @@
+const map = [
+  "WWWWWWWWWWWWWWWWWWWWW",
+  "W   W     W     W   W",
+  "W W W WWW WWWWW W WWW",
+  "W W W  W      W W   W",
+  "W WWWWWWW W WWW W W W",
+  "W         W     W W W",
+  "W WWW WWWWW WWWWW W W",
+  "W W   W   W W     W W",
+  "W WWWWW W W W WWW W F",
+  "S     W W W W W W WWW",
+  "WWWWW W W W W W W W W",
+  "W     W W W   W W W W",
+  "W WWWWWWW WWWWW W W W",
+  "W       W       W   W",
+  "WWWWWWWWWWWWWWWWWWWWW"
+];
+
+// Your Code Here.
